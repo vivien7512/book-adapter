b7d69d8212e8c39faeae61e40cb9b5fff6c525d2
Vivien Pineau <vivien.pineau@gmail.com>
2024-05-30 04:34:54 +0200

    improve style

--- frontend-book-adapter/src/App.vue
+++ frontend-book-adapter/src/App.vue
@@ -1,92 +1,164 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-// Déclaration de variables réactives
-const message = ref('Bienvenue à Vue 3 avec TypeScript!')
-
-</script>
-
 <template>
-  <div id="root">
-    <nav>
-      <span class="nav-item ripple"><router-link to="/PageEditor">PAGE Editor</router-link></span>
-      <span class="nav-item ripple"><router-link to="/tab2">2</router-link></span>
-      <span class="nav-item ripple"><router-link to="/tab3">3</router-link></span>
-    </nav>
-    <router-view />
+  <div id="page-editor" class="min-h-screen bg-gray-100 p-4 flex flex-col">
+    <div class="bg-white shadow-md rounded-lg p-6 flex flex-col flex-grow w-full">
+      <div id="toolbar" class="flex items-center space-x-4 mb-4 w-full">
+        <select v-model="currentBookName" class="p-2 border border-gray-300 rounded flex-grow">
+          <option v-for="bookName in booksNames" :key="bookName" :value="bookName">{{ bookName }}</option>
+        </select>
+        <button @click="saveText" :disabled="!isTextUpdated" class="bg-blue-500 text-white px-4 py-2 rounded disabled:opacity-50">🖫 Save</button>
+      </div>
+      <div id="editor" class="flex-grow flex space-x-4 w-full">
+        <div id="image-viewer" class="flex-1 bg-gray-200 rounded-lg overflow-hidden">
+          <img v-if="currentItem" :src="`http://localhost:3000/api/getBookImage/${currentBookName}/${currentItem.name}.png`" alt="Image not found" class="w-full h-auto" />
+        </div>
+        <div id="text-editor" class="flex-1">
+          <textarea v-model="currentText" name="text-editor" id="text-editor" cols="30" rows="10" class="w-full h-full p-2 border border-gray-300 rounded"></textarea>
+        </div>
+      </div>
+      <div id="navigator-bar" class="flex items-center justify-between mt-4 w-full">
+        <button @click="prevInfo" class="bg-gray-300 text-gray-700 px-4 py-2 rounded">← Prev</button>
+        <div id="page-name" class="text-center flex-grow">
+          <div>
+            <span v-if="currentItem" class="font-semibold">{{ currentItem.name }}</span>
+          </div>
+          <div>
+            <span v-if="currentItem">{{ currentItem.index }} / {{ metaInfos.length }}</span>
+          </div>
+        </div>
+        <button @click="nextInfo" class="bg-gray-300 text-gray-700 px-4 py-2 rounded">Next →</button>
+      </div>
+    </div>
+    <!-- Loader -->
+    <div v-if="isLoading" class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
+      <div class="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-#root {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  width: 100vw;
-  margin: 0;
-  background-color: #008B8B;
-  overflow: hidden; 
+<script setup lang="ts">
+import { ref, onMounted, computed, watch } from 'vue'
+import axios from 'axios'
+
+interface MetaInfo {
+  name: string;
+  index: number;
 }
 
-nav {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  background-color: #008B8B;
-  height: 50px;
+const metaInfos = ref<MetaInfo[]>([])
+const currentIndex = ref<number>(1)
+const currentText = ref<string>('')
+const isLoading = ref<boolean>(false)  
+const booksNames = ref<string[]>([])
+const currentBookName = ref<string>('')
+const isTextUpdated = ref<boolean>(false)
+let isLoadingText = false; 
+
+const fetchMetasInfos = async (newBookName: string) => {
+  try {
+    isLoading.value = true  
+    const response = await axios.get(`http://localhost:3000/api/getBookMetaInfos?bookName=${newBookName}`)
+    metaInfos.value = response.data
+    if (metaInfos.value.length > 0) {
+      await getTextOfImage(metaInfos.value[0].name)
+    }
+  } catch (error) {
+    console.error('Erreur lors de la récupération des metaInfos:', error)
+  } finally {
+    isLoading.value = false 
+  }
 }
 
-.nav-item {
-  font-size: 16px;
-  color: rgba(255, 255, 255, 0.7);
-  font-weight: bold;
-  text-transform: uppercase;
-  text-decoration: none;
-  padding: 10px 20px;
-  position: relative;
-  text-align: center;
-  flex-grow: 1;
+const getTextOfImage = async (imageName: string) => {
+  try {
+    let bookName = currentBookName.value
+    isLoading.value = true  
+    isLoadingText = true; // Début du chargement du texte
+    let request = `http://localhost:3000/api/getTextOfImage?bookName=${bookName}&imageName=${imageName}`
+    const response = await axios.get(request)
+    currentText.value = response.data.text
+    isTextUpdated.value = false
+  } catch (error) {
+    console.error('Erreur lors de la récupération de l\'image et du texte:', error)
+  } finally {
+    isLoading.value = false
+    new Promise(resolve => setTimeout(resolve, 1000)).then(() => isLoadingText = false);
+ 
+  }
 }
 
-.nav-item a {
-  color: inherit;
-  text-decoration: none;
-  width: 100%;
-  display: inline-block;
+const fetchBooksNames = async () => {
+  try {
+    isLoading.value = true  
+    const response = await axios.get('http://localhost:3000/api/getBooksNames')
+    booksNames.value = response.data
+  } catch (error) {
+    console.error('Erreur lors de la récupération des noms des livres:', error)
+  } finally {
+    isLoading.value = false 
+  }
 }
 
-.router-link-active {
-  color: white;
-  font-weight: bold;
+const saveText = async () => {
+  if (!currentItem.value) return
+  try {
+    isLoading.value = true  
+    await axios.post('http://localhost:3000/api/updateTextOfPage', {
+      bookName: currentBookName.value,
+      imageName: currentItem.value.name,
+      newText: currentText.value
+    })
+    isTextUpdated.value = false
+  } catch (error) {
+    console.error('Erreur lors de la sauvegarde du texte:', error)
+  } finally {
+    isLoading.value = false 
+  }
 }
 
-.router-link-active::after {
-  content: '';
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 2px;
-  background-color: white;
+onMounted(() => {
+  isTextUpdated.value = false
+  fetchBooksNames()
+})
+
+watch(currentIndex, (newIndex) => {
+  const metaInfo = metaInfos.value[newIndex - 1]
+  if (metaInfo) {
+    getTextOfImage(metaInfo.name)
+  }
+})
+
+watch(currentBookName, (newBookName) => {
+  fetchMetasInfos(newBookName)
+})
+
+// Watch currentText to set isTextUpdated to true when it changes
+watch(currentText, (newText, oldText) => {
+  if (!isLoadingText && newText !== oldText) {
+    isTextUpdated.value = true
+  }
+})
+
+const currentItem = computed(() => metaInfos.value[currentIndex.value - 1])
+
+const prevInfo = () => {
+  if (currentIndex.value > 1) {
+    currentIndex.value--
+  }
 }
 
-.nav-item:hover {
-  background-color: #006F6F;
-  border-radius: 5px;
+const nextInfo = () => {
+  if (currentIndex.value < metaInfos.value.length) {
+    currentIndex.value++
+  }
 }
+</script>
 
-.ripple {
-  background-position: center;
-  transition: background 0.8s;
+<style scoped>
+.loader {
+  border-top-color: #3498db;
+  animation: spin 1s ease-in-out infinite;
 }
-
-.ripple:hover {
-  background: #006F6F radial-gradient(circle, transparent 1%, #006F6F 1%) center/15000%;
-}
-
-.ripple:active {
-  background-color: #008B8B;
-  background-size: 100%;
-  transition: background 0s;
+@keyframes spin {
+  to { transform: rotate(360deg); }
 }
 </style>
--- frontend-book-adapter/src/main.ts
+++ frontend-book-adapter/src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import './assets/tailwind.css' // Ajoutez cette ligne
+
 
 createApp(App).use(router).mount('#app')
